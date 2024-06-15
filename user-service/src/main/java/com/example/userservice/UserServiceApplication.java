5f746c9ed73afa4050d9bd6d7785a53783a44815
andrea285 <andrea.lojpur285@gmail.com>
2024-06-15 16:29:40 +0200

    Revert "Update security and user config"
    This reverts commit 5bd65ac3c4c4c35335f1fa486fb88de79bad8dd7.

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.userservice"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/example/userservice/model/User.java
+++ user-service/src/main/java/com/example/userservice/model/User.java
@@ -7,10 +7,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String id;
-    private String username;
-    private String password;
+    private String name;
     private String email;
-    private String role;
 
     // Getteri i setteri
     public String getId() {
@@ -21,20 +19,12 @@ public class User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -43,13 +33,5 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 }
